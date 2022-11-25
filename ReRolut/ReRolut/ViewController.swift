354246f92f79dec76b3bb0f5473096fae801a7e5
Rolandas-R <driveris@gmail.com>
2022-11-25 20:09:06 +0200

    Created local repository, started project in Xcode,  settings
    Created local repository for CodeAcademy 1st Level Final Assignment: sophisticated points transfer iOS app called ReRolut.
    Created prjoect in Xcode. Changed app settings: portred mode only, iPhone only, minimum iOS 15

---
+++ ReRolut/ReRolut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReRolut
+//
+//  Created by reromac on 2022-11-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
