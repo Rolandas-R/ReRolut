8fd7656dfdb5d0ce0772046473184561597029d6
Rolandas-R <driveris@gmail.com>
2022-11-26 13:32:38 +0200

    segment controller action logic, UI constraints improvements
    Added segmented controller action logic, UI button label text setting logic and UI text field hiding actions depending on userLoginSegmentedController.selectedSegmentIndex.

--- ReRolut/ReRolut/ViewController.swift
+++ ReRolut/ReRolut/ViewController.swift
@@ -5,13 +5,23 @@
 //  Created by reromac on 2022-11-25.
 //
 
+// TODO: - pakeisti logo paveiksliuka i ReRolut
+// TODO: - userio issaugojimas ir visa userMAnager logika per klases User ir userManager
+// TODO: - userio validavimas ir erroru alertai
+
 import UIKit
 
 class ViewController: UIViewController {
     
+    // enumas, kuriame nurodoma, kad pradinis langas tures dvi busenas (segmentus): registravimosi ir prisijungimo
+    enum Segment {
+        case register
+        case login
+    }
+    
     @IBOutlet private weak var imageViewPersonLogo: UIImageView!
     
-    @IBOutlet weak var userLoginSegmentedController: UISegmentedControl!
+    @IBOutlet private weak var userLoginSegmentedController: UISegmentedControl!
     
     @IBOutlet private weak var enterUsernameTextField: UITextField!
     
@@ -21,6 +31,8 @@ class ViewController: UIViewController {
     
     @IBOutlet private weak var loginButton: UIButton!
     
+    var currentSegment: Segment = .register
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,20 +40,44 @@ class ViewController: UIViewController {
     }
     
     
+    /* segmentinio kontrolerio veikimo f-ja. joje atsizvelgiant i pasirinkta busena (.login ar .register) parodoma ar bus isvedamas passwordo konfirminimo teksto laukas. Taip pat switcho pagalba, atsizvelgiant i segmenta, parenkamas rodomo labelio ant mygtuko uzrasas */
     @IBAction func userLoginSegmentedControllerStateChanged(_ sender: Any) {
         
-        //patikrinimas kuris segmentas pasirinktas
+        if userLoginSegmentedController.selectedSegmentIndex == 0 {
+            currentSegment = .register
+        } else if userLoginSegmentedController.selectedSegmentIndex == 1 {
+            currentSegment = .login
+        }
+        
+        /* jeigu pasirinktas segmentas yra .login tai passwordo retypinimo textfieldas nerodomas */
+        retypePasswordTextField.isHidden = currentSegment != .register
+        
+        
+        // switchas kurio pagalba isvedamas uzrasas ant mygtuko:
+        switch currentSegment {
+        case .register:
+            loginButton.setTitle("REGISTER", for: .normal)
+        case .login:
+            loginButton.setTitle("LOGIN", for: .normal)
+            
+            
+        //patikrinimas kuris segmentas pasirinktas (nebutinas)
         print(userLoginSegmentedController.selectedSegmentIndex)
+        }
+
     }
+
+    /* mygtuko paspaudimas - atliekamos f-jos:
+     */
     
     @IBAction func loginButtonTapped(_ sender: Any) {
         
-        //patikrinimas ar veikia duomenu ivedimas
+        //patikrinimas ar veikia duomenu ivedimas (nebutinas)
         print(enterUsernameTextField.text!)
         print(enterPasswordTextField.text!)
     }
     
-
-
+    
+    
 }
 
