c5b327c8dcfbfac762722f6b048e4ac55b5126b4
Rolandas-R <driveris@gmail.com>
2022-11-26 11:31:32 +0200

    created outlets in ViewController, created action for button,
    Outlets for username, password, retype password textFields and imageView person.emptylog outlet. Created outlet for loginButton, also for this button created action loginButtonTapped. With prints checked how all outlets and action performs

--- ReRolut/ReRolut/ViewController.swift
+++ ReRolut/ReRolut/ViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private weak var imageViewPersonLogo: UIImageView!
+    
+    @IBOutlet private weak var enterUsernameTextField: UITextField!
+    
+    @IBOutlet private weak var enterPasswordTextField: UITextField!
+    
+    @IBOutlet private weak var retypePasswordTextField: UITextField!
+    
+    @IBOutlet private weak var loginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        print(enterUsernameTextField.text!)
+        print(enterPasswordTextField.text!)
+    }
+    
 
 
 }
